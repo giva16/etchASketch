56f7d17c320d20b08ab2c9018160544817351798
giva16 <ltpie1@hotmail.com>
2023-12-27 23:53:21 +0100

    Feature: Rainbow Mode. Added rainbow mode taggle to enable user to fill the grid with randomized colros.

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 // Global state
 let mouseIsDown = false;
+let rainbowMode = false;
 
 // UI
 const root = document.querySelector('html');
 const container = document.querySelector('.grid-container');
 const clearBtn = document.querySelector('#btnClear');
 const createBtn = document.querySelector('#btnCreate');
+const rainbowBtn = document.querySelector('#btnRainbow');
 
 // Event Listeners
 root.addEventListener('mousedown', () => {mouseIsDown = true;})
@@ -13,6 +15,7 @@ root.addEventListener('mouseup', () => {mouseIsDown = false;})
 container.addEventListener('mouseover', fillSquare);
 clearBtn.onclick = clearGrid;
 createBtn.onclick = renderCustomGrid;
+rainbowBtn.onclick = toggleRainbow;
 
 // Grid Rendering
 function createSquare(row) {
@@ -24,7 +27,7 @@ function createSquare(row) {
 function clearGrid(){
     for (row of container.children){
         for (square of row.children){
-           square.classList.remove('fill');
+           square.style.backgroundColor = 'white';
         }
     }
 }
@@ -42,9 +45,7 @@ function renderGrid(dim) {
 }
 
 // USER CONTROLS
-function resetCanvas() {
-    container.innerHTML = '';
-}
+function resetCanvas() {container.innerHTML = '';}
 
 // row to contain squares in a flex container that will ensure even space distribution
 function createRow() {
@@ -57,18 +58,41 @@ function createRow() {
 // adds 'fill' class to fill a square with black background
 function fillSquare(event) {
     if (event.target.classList.contains('square') && mouseIsDown === true) {
-        event.target.classList.add('fill');
+        if (rainbowMode){
+            event.target.style.backgroundColor = randomHexColor();
+        } else {
+            event.target.style.backgroundColor = 'black';
+        }
     }
 }
 
 // accept user input in order to render a custom dimension grid
 function renderCustomGrid() {
     let dim = prompt("Enter a grid size: ");
-    
     while (!Number.isInteger(parseInt(dim)) || (dim % 1) != 0){
         dim = prompt("Invalid Input! Enter a grid size again (whole number): ");
     }
     renderGrid(parseInt(dim));
 }
+
+function randomVal(max) {return Math.floor(Math.random() * max) + 1;}
+
+// Get Random Colors
+function getRandomColor() {return [randomVal(255), randomVal(255), randomVal(255)];}
+
+function randomHexColor() {
+    let [r, g, b] = getRandomColor();
+    let hr = r.toString(16).padStart(2, '0');
+    let hg = g.toString(16).padStart(2, '0');
+    let hb = b.toString(16).padStart(2, '0');
+    return "#" + hr + hg + hb;
+}
+
+function toggleRainbow() {
+    if (rainbowMode) {rainbowMode = false;}
+    else {rainbowMode = true;}
+}
+
+
 
 renderGrid(20);
