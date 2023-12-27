d07c79353e53b0b5f2929b71aeb001c159b5e9fc
giva16 <ltpie1@hotmail.com>
2023-12-27 08:25:31 +0100

    Feature: Drawing Functionality Added. Users can now draw on the grid by holding the primary button of a mouse and hovering over the squares within the grid

--- script.js
+++ script.js
@@ -7,6 +7,7 @@
 
 
 // select container for grid rows
+const root = document.querySelector('html');
 const container = document.querySelector('.grid-container');
 
 function createSquare(row) {
@@ -15,6 +16,7 @@ function createSquare(row) {
     row.appendChild(square);
 }
 
+// row to contain squares in a flex container that will ensure even space distribution
 function createRow() {
     let row = document.createElement('div');
     row.classList.add('row');
@@ -32,3 +34,19 @@ function renderGrid(dim) {
         container.appendChild(row);
     }
 }
+
+// draw function keeps track of mouseUp and mouseDown events from the root of the document
+// so that the squres are only filled when the mouse left button is held
+function draw() {
+    let mouseIsDown = false;
+    root.addEventListener('mousedown', function(){mouseIsDown = true});
+    root.addEventListener('mouseup', function() {mouseIsDown = false});
+    container.addEventListener('mouseover', function(e){
+        if (e.target.classList.contains('square') && mouseIsDown === true) {
+            e.target.classList.add('fill');
+        }
+    });
+}
+
+renderGrid(64);
+draw();
